445e2abc5ffaac386b1c0850bb820b2dccd87c47
Mohamed amine Mrhili <mohamed-amine-mrhili@hotmail.com>
2019-05-19 17:23:52 +0200

    message services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,34 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[];
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
+
+  }
 
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  ngOnInit() {
-  }
+
 
 
 
